7b9a1c5a818e58c8a64aaa3d2c978ff6d71ccb88
Tommy Lees <thomas.lees112@gmail.com>
2021-10-28 15:28:01 +0100

    formatter update

--- src/w2w/data/build_zarchive.py
+++ src/w2w/data/build_zarchive.py
@@ -1,8 +1,9 @@
-from typing import Tuple
+from typing import Tuple, List
 import glob, os
 from datetime import datetime as dt
 import multiprocessing as mp
 import logging
+
 logging.basicConfig(level=logging.INFO)
 
 import zarr
@@ -15,151 +16,163 @@ import numpy as np
 from workers import write_worker_mp
 
 VARIABLES = {
-    't2m':'2m_temperature',
-    'u10':'10m_u_component_of_wind',
-    'v10':'10m_v_component_of_wind',
-    'tp':'total_precipitation',
-    'potev':'potential_evaporation',
+    "t2m": "2m_temperature",
+    "u10": "10m_u_component_of_wind",
+    "v10": "10m_v_component_of_wind",
+    "tp": "total_precipitation",
+    "potev": "potential_evaporation",
 }
 
 FORECAST_VARIABLES = {
-    't2m':'2m_temperature',
-    'tp':'total_precipitation',
+    "t2m": "2m_temperature",
+    "tp": "total_precipitation",
 }
 
+
 def build_forecast_archive(
-    start_date: str,
-    end_date: str,
-    chunks: Tuple[int,int,int,int],
+    start_date: str,  # = '2010-01-01',
+    end_date: str,  # = '2024-12-31',
+    chunks: Tuple[int, int, int, int],
     gcs_root: str,
 ) -> int:
-    
-    dt_range = pd.date_range('2010-01-01', end='2024-12-31', freq='1d')
-    steps = pd.date_range('2010-01-01', periods=61,freq='6H') - pd.Timestamp('2010-01-01')
-    
+
+    dt_range = pd.date_range(start_date, end=end_date, freq="1d")
+    # forecast horizon = 15 days every 6 hours
+    steps = pd.date_range(start_date, periods=61, freq="6H") - pd.Timestamp(start_date)
+
     # first, build the coordinates
     coords = {
-        'longitude': np.arange(0,360,0.5),
-        'latitude': np.arange(90,-90,-0.5),
-        'time': dt_range,
-        'step': steps,
+        "longitude": np.arange(0, 360, 0.5),
+        "latitude": np.arange(90, -90, -0.5),
+        "time": dt_range,
+        "step": steps,
     }
-    
+
     # next, build the variables
     # use a dask array hold the dummy dimensions
-    dummies = dask.array.zeros((720, 360, dt_range.shape[0], steps.shape[0]), chunks=chunks)  # lon, lat, day, steps
-    
+    dummies = dask.array.zeros(
+        (720, 360, dt_range.shape[0], steps.shape[0]), chunks=chunks
+    )  # lon, lat, day, steps
+
     # mock-up the dataset
     ds = xr.Dataset(
         {kk: (tuple(coords.keys()), dummies) for kk in FORECAST_VARIABLES.keys()},
-        coords = coords
+        coords=coords,
     )
-    
+
     # set up the Google Cloud Storage store
     store = gcsfs.GCSMap(root=gcs_root)
-    
+
     # Now we write the metadata without computing any array values
     ds.to_zarr(store, compute=False, consolidated=True)
-    
+
     return 1
-    
-    
-    
+
 
 def build_zarr_archive(
-    start_date: str,
-    end_date: str,
-    chunks: Tuple[int,int,int],
-    gcs_root: str,
+    start_date: str, end_date: str, chunks: Tuple[int, int, int], gcs_root: str,
 ) -> int:
-    
-    dt_range = pd.date_range(start_date, end=end_date,freq='1H')
-    
+
+    dt_range = pd.date_range(start_date, end=end_date, freq="1H")
+
     # first, build the coordinates
     coords = {
-        'longitude': np.arange(0,360,0.1),
-        'latitude': np.arange(90,-90,-0.1),
-        'time': dt_range
+        "longitude": np.arange(0, 360, 0.1),
+        "latitude": np.arange(90, -90, -0.1),
+        "time": dt_range,
     }
-    
+
     # next, build the variables
     # use a dask array hold the dummy dimensions
-    dummies = dask.array.zeros((3600,1800,dt_range.shape[0]), chunks=chunks)
-    
+    dummies = dask.array.zeros(
+        (len(coords["longitude"]), len(coords["latitude"]), dt_range.shape[0]),
+        chunks=chunks,
+    )
+
     # mock-up the dataset
     ds = xr.Dataset(
-        {kk: (tuple(coords.keys()), dummies) for kk in VARIABLES.keys()},
-        coords = coords
+        {kk: (tuple(coords.keys()), dummies) for kk in VARIABLES.keys()}, coords=coords
     )
-    
+
     # set up the Google Cloud Storage store
     store = gcsfs.GCSMap(root=gcs_root)
-    
+
     # Now we write the metadata without computing any array values
     ds.to_zarr(store, compute=False, consolidated=True)
-    
-    return 1
 
+    return 1
 
 
 def push_month(gcs_root, f, n_workers):
-    
-    year, month, var = os.path.splitext(os.path.split(f)[1])[0].split('_')
-    
-    logger = logging.getLogger(f'PUSH-{year}-{month}-{var}')
-    
+
+    year, month, var = os.path.splitext(os.path.split(f)[1])[0].split("_")
+
+    logger = logging.getLogger(f"PUSH-{year}-{month}-{var}")
+
     # get the ds object
-    ds = xr.open_dataset(f,chunks={'longitude':10, 'latitude':10, 'time':35064})
-    
+    ds = xr.open_dataset(f, chunks={"longitude": 10, "latitude": 10, "time": 35064})
+
     # get slices to write
-    slices = dask.array.core.slices_from_chunks(dask.array.empty_like(ds.to_array()).chunks) # null, time, lat, lon
-    
+    slices = dask.array.core.slices_from_chunks(
+        dask.array.empty_like(ds.to_array()).chunks
+    )  # null, time, lat, lon
+
     # eliminate the slices which hit the boundary
-    slices = [s for s in slices if s[2].stop!=1801]
-    logger.info(f'Pushing {len(slices)}')
-    
+    slices = [s for s in slices if s[2].stop != 1801]
+    logger.info(f"Pushing {len(slices)}")
+
     # prep for multiprocessing
-    chunk_worker = len(slices)//n_workers+1
-    slices_rechunked = [slices[chunk_worker*ii:chunk_worker*(ii+1)] for ii in range(n_workers)]
-    
+    chunk_worker = len(slices) // n_workers + 1
+    slices_rechunked = [
+        slices[chunk_worker * ii : chunk_worker * (ii + 1)] for ii in range(n_workers)
+    ]
+
     # prep the pool for writing
     pool = mp.Pool(n_workers)
-    
-    time_offset = int((dt.strptime(f'{year}-{month}-01', '%Y-%m-%d') - dt.strptime('1981-01-01','%Y-%m-%d')).total_seconds()/3600)
-    
-    args = [
+
+    time_offset = int(
         (
-        f, 
-        var, 
-        gcs_root, 
-        slices_rechunked[ii], 
-        time_offset,
-        ii
-        ) for ii in range(n_workers)
+            dt.strptime(f"{year}-{month}-01", "%Y-%m-%d")
+            - dt.strptime("1981-01-01", "%Y-%m-%d")
+        ).total_seconds()
+        / 3600
+    )
+
+    args = [
+        (f, var, gcs_root, slices_rechunked[ii], time_offset, ii)
+        for ii in range(n_workers)
     ]
 
     results = pool.starmap(write_worker_mp, args)
-    
+
     return np.prod(results)
 
 
-def loop(fs, gcs_root, n_workers):
-    
+def loop(fs: List[str], gcs_root: str, n_workers: int):
     for f in fs:
-        print (f)
-        push_month(gcs_root,f,n_workers)
-        
-        
+        print(f)
+        push_month(gcs_root, f, n_workers)
+
+
 def scheduler():
-    # a scheduler. Let's do a full year-variable at a time, and then 
+    # a scheduler. Let's do a full year-variable at a time, and then
     pass
 
-if __name__=="__main__":
-    gcs_root = 'oxeo-forecasts/ecmwf-tigge-15day'
-    
+
+if __name__ == "__main__":
+    gcs_root = "oxeo-forecasts/ecmwf-tigge-15day"
+
     build_forecast_archive(
-        start_date = '2010-01-01 00:00:00',
-        end_date = '2024-12-31 23:00:00',
-        chunks = (10, 10, 1461, 61), # lon, lat, day, steps
-        gcs_root = gcs_root
+        start_date="2010-01-01 00:00:00",
+        end_date="2024-12-31 23:00:00",
+        chunks=(10, 10, 1461, 61),  # lon, lat, day, steps
+        gcs_root=gcs_root,
     )
+
+    # gcs_root = "oxeo-era5/lk-test-build"
+    # build_zarr_archive(
+    #     start_date=pd.to_datetime("1984-01-01"),
+    #     end_date=pd.to_datetime("2014-12-31"),
+    #     chunks=(10, 10, 1461),
+    #     gcs_root=gcs_root
+    # )
--- src/w2w/data/workers.py
+++ src/w2w/data/workers.py
@@ -1,4 +1,5 @@
 import logging
+
 logging.basicConfig(level=logging.INFO)
 
 import netCDF4 as nc
@@ -15,126 +16,134 @@ from ecmwfapi import ECMWFDataServer
 
 
 def sharedmem_worker(shm_spec, variable, gcs_path, slices, time_offset, worker_idx):
-    
-    logger = logging.getLogger(f'worker_{worker_idx}')
-    
+
+    logger = logging.getLogger(f"worker_{worker_idx}")
+
     # open the dataset from sharedmemory
-    existing_shm = shared_memory.SharedMemory(name=shm_spec['name'])
-    data = np.ndarray(shm_spec['shape'], dtype=shm_spec['dtype'], buffer=existing_shm.buf)
-    
+    existing_shm = shared_memory.SharedMemory(name=shm_spec["name"])
+    data = np.ndarray(
+        shm_spec["shape"], dtype=shm_spec["dtype"], buffer=existing_shm.buf
+    )
+
     # open the zarr
     store = gcsfs.GCSMap(root=gcs_path)
     z = zarr.open(store)
-    
+
     # write each slice
-    for ii_s,s in enumerate(slices):
-        if ii_s % 100 ==0:
-            logger.info(f'ii_s:{ii_s}')
-            
+    for ii_s, s in enumerate(slices):
+        if ii_s % 100 == 0:
+            logger.info(f"ii_s:{ii_s}")
+
         time_slice = s[1]
-        offset_slice = slice(s[1].start+time_offset, s[1].stop+time_offset)
+        offset_slice = slice(s[1].start + time_offset, s[1].stop + time_offset)
         step_slice = s[2]
         lat_slice = s[3]
         lon_slice = s[4]
-        z[variable][lon_slice, lat_slice, offset_slice, step_slice] = data[lon_slice, lat_slice, time_slice, step_slice]
-        
-        
-    #del data  # Unnecessary; merely emphasizing the array is no longer used
+        z[variable][lon_slice, lat_slice, offset_slice, step_slice] = data[
+            lon_slice, lat_slice, time_slice, step_slice
+        ]
+
+    # del data  # Unnecessary; merely emphasizing the array is no longer used
     existing_shm.close()
-        
+
     return 1
 
 
 def ecmwf_caller(fname):
-    
-    year, month = os.path.splitext(os.path.split(fname)[1])[0].split('_')
-    
-    if int(month)==2:
-        if year in [2008,2012,2016, 2020]:
+
+    year, month = os.path.splitext(os.path.split(fname)[1])[0].split("_")
+
+    if int(month) == 2:
+        if year in [2008, 2012, 2016, 2020]:
             last_day = 29
         else:
             last_day = 28
-    elif int(month) in [1,3,5,7,8,10,12]:
+    elif int(month) in [1, 3, 5, 7, 8, 10, 12]:
         last_day = 31
     else:
-        last_day=30
-    
-    print (f'getting {fname}: {year} {month}')
-    
+        last_day = 30
+
+    print(f"getting {fname}: {year} {month}")
+
     server = ECMWFDataServer()
 
-    server.retrieve({
-        "class": "ti",
-        "dataset": "tigge",
-        "date": f"{year}-{month}-01/to/{year}-{month}-{last_day:02d}",
-        "expver": "prod",
-        "grid": "0.5/0.5",
-        "levtype": "sfc",
-        "origin": "ecmf",
-        "param": "167/228228",
-        "step": "0/6/12/18/24/30/36/42/48/54/60/66/72/78/84/90/96/102/108/114/120/126/132/138/144/150/156/162/168/174/180/186/192/198/204/210/216/222/228/234/240/246/252/258/264/270/276/282/288/294/300/306/312/318/324/330/336/342/348/354/360",
-        "time": "00:00:00",
-        "type": "cf",
-        "target": fname,
-    })
-    
+    server.retrieve(
+        {
+            "class": "ti",
+            "dataset": "tigge",
+            "date": f"{year}-{month}-01/to/{year}-{month}-{last_day:02d}",
+            "expver": "prod",
+            "grid": "0.5/0.5",
+            "levtype": "sfc",
+            "origin": "ecmf",
+            "param": "167/228228",
+            "step": "0/6/12/18/24/30/36/42/48/54/60/66/72/78/84/90/96/102/108/114/120/126/132/138/144/150/156/162/168/174/180/186/192/198/204/210/216/222/228/234/240/246/252/258/264/270/276/282/288/294/300/306/312/318/324/330/336/342/348/354/360",
+            "time": "00:00:00",
+            "type": "cf",
+            "target": fname,
+        }
+    )
+
     return 1
 
 
-def write_worker_mp(filename, variable, gcs_path, slices, time_offset, worker_idx):
-    
-    logger = logging.getLogger(f'worker_{worker_idx}')
-    
+def write_worker_mp(
+    filename: str, variable, gcs_path, slices, time_offset, worker_idx
+) -> int:
+
+    logger = logging.getLogger(f"worker_{worker_idx}")
+
     # open the dataset and the zarr
     ds_worker = nc.Dataset(filename)
     store = gcsfs.GCSMap(root=gcs_path)
     z = zarr.open(store)
-    
+
     # write each slice
-    for ii_s,subslice in enumerate(slices):
-        if ii_s % 100 ==0:
-            logger.info(f'ii_s:{ii_s}')
+    for ii_s, subslice in enumerate(slices):
+        if ii_s % 100 == 0:
+            logger.info(f"ii_s:{ii_s}")
         time_slice = subslice[1]
-        offset_slice = slice(subslice[1].start+time_offset, subslice[1].stop+time_offset)
+        offset_slice = slice(
+            subslice[1].start + time_offset, subslice[1].stop + time_offset
+        )
         lat_slice = subslice[2]
         lon_slice = subslice[3]
         z[variable][lon_slice, lat_slice, offset_slice] = np.transpose(
-            np.squeeze(
-                ds_worker[variable][time_slice, lat_slice, lon_slice]
-            ),
-            [2,1,0]
+            np.squeeze(ds_worker[variable][time_slice, lat_slice, lon_slice]), [2, 1, 0]
         )
-        
-        
+
     return 1
 
+
 def write_worker_ecmwf(filename, gcs_path, slices, worker_idx):
-    
-    logger = logging.getLogger(f'worker_{worker_idx}')
-    
-    year, month = os.path.splitext(os.path.split(filename)[1])[0].split('_')
-    
-    time_offset = (dt(year=int(year), month=int(month), day=1) - dt(year=2010,month=1,day=1)).days
-    
-    ds_worker = xr.open_dataset(filename,engine='cfgrib')
-    
+
+    logger = logging.getLogger(f"worker_{worker_idx}")
+
+    year, month = os.path.splitext(os.path.split(filename)[1])[0].split("_")
+
+    time_offset = (
+        dt(year=int(year), month=int(month), day=1) - dt(year=2010, month=1, day=1)
+    ).days
+
+    ds_worker = xr.open_dataset(filename, engine="cfgrib")
+
     store = gcsfs.GCSMap(root=gcs_path)
     z = zarr.open(store)
-    
+
     variables = {
-        0:'t2m',
-        1:'tp',
+        0: "t2m",
+        1: "tp",
     }
-    
+
     for ii_s, s in enumerate(slices):
-        
+
         variable = variables[s[0].start]
-        
-        if ii_s % 10==0:
-            logger.info(f'ii_s:{ii_s}')
-        
+
+        if ii_s % 10 == 0:
+            logger.info(f"ii_s:{ii_s}")
+
         time_slice = s[1]
-        offset_slice = slice(s[1].start+time_offset, s[1].stop+time_offset)
+        offset_slice = slice(s[1].start + time_offset, s[1].stop + time_offset)
         step_slice = s[2]
         lat_slice = s[3]
         lon_slice = s[4]
@@ -142,10 +151,5 @@ def write_worker_ecmwf(filename, gcs_path, slices, worker_idx):
             np.squeeze(
                 ds_worker[variable][time_slice, step_slice, lat_slice, lon_slice]
             ),
-            [3,2,0,1]
+            [3, 2, 0, 1],
         )
-        
-    
-    
-    
-    
