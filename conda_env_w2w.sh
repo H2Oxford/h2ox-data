a280155581bc52cf955fa095120932459683a383
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2021-11-03 04:18:57 +0000

    refactor rm files

--- conda_env_w2w.sh
+++
@@ -1,8 +0,0 @@
-conda create -n w2w python=3.8 --yes 
-conda activate w2w
-conda install -c conda-forge pytorch xarray=0.16 pytorch torchvision cudatoolkit --yes
-conda install dask --yes
-conda install -c conda-forge rasterio scikit-image --yes
-conda install -c conda-forge seaborn=0.11 --yes
-conda install -c anaconda networkx --yes
-conda install -c conda-forge netcdf4 numba tqdm jupyterlab tensorboard ipython pip ruamel.yaml descartes statsmodels scikit-learn black mypy --yes
