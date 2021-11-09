3c26aa3f4e67f747bac644f80b63cd10647759a4
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2021-11-09 22:40:19 +0000

    add setup and dockerfile

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# Use the official osgeo/gdal image.
+FROM osgeo/gdal:ubuntu-small-latest
+
+# Allow statements and log messages to immediately appear in the Knative logs
+ENV PYTHONUNBUFFERED True
+
+# Set provider dir path
+ENV PROVIDER GCP
+
+ENV APP_HOME /app
+
+ENV MAIN apps/ecmwf_downloader
+
+COPY $MAIN ./
+
+WORKDIR $APP_HOME
+
+# Copy local code to the container image.
+# __context__ to __workdir__ 
+COPY . ./h2ox-data
+COPY ./src/h2ox/provider/requirements/$PROVIDER/requirements.txt ./
+# Install GDAL dependencies
+RUN apt-get update
+RUN apt-get install -y python3-pip
+# Install production dependencies.
+RUN pip install --no-cache-dir ./h2ox-data
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Run the web service on container startup. Here we use the gunicorn
+# webserver, with one worker process and 8 threads.
+# For environments with multiple CPU cores, increase the number of workers
+# to be equal to the cores available.
+# Timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling.
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app
---
+++ setup.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+import io
+import shlex
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+from subprocess import check_call
+
+from setuptools import find_packages
+from setuptools import setup
+from setuptools.command.develop import develop
+
+
+class PostDevelopCommand(develop):
+    def run(self):
+        try:
+            check_call(shlex.split("pre-commit install"))
+        except Exception as e:
+            print(f"Unable to run 'pre-commit install' with exception {e}")
+        develop.run(self)
+
+
+def read(*names, **kwargs):
+    with io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as fh:
+        return fh.read()
+
+
+setup(
+    name="h2ox-data",
+    version="0.1.0",
+    license="BSD-2-Clause",
+    description="h2ox-data. Collect data from ECMWF.",
+    url="https://github.com/H2Oxford/h2ox-data.git",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        # uncomment if you test on these interpreters:
+        # 'Programming Language :: Python :: Implementation :: IronPython',
+        # 'Programming Language :: Python :: Implementation :: Jython',
+        # 'Programming Language :: Python :: Implementation :: Stackless',
+        "Topic :: Utilities",
+    ],
+    project_urls={
+        "Documentation": "https://h2oxford.readthedocs.io/",
+        "Changelog": "https://h2oxford.readthedocs.io/en/latest/changelog.html",
+        "Issue Tracker": "https://github.com/H2Oxford/h2ox-data/issues",
+    },
+    keywords=[
+        # eg: 'keyword1', 'keyword2', 'keyword3',
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "loguru~=0.5.3",
+        "joblib~=1.1.0",
+        "numpy~=1.21.4",
+        "pandas~=1.3.4",
+        "shapely~=1.8.0",
+        "gunicorn~=20.1.0",
+        "pyproj~=3.2.1",
+        "geopandas~=0.10.2",
+        "rasterio~=1.2.10",
+        "zarr~=2.10.2",
+        "tqdm~=4.62.3",
+    ],
+    extras_require={
+        "dash": [
+            "dash~=2.0.0",
+            "dash-auth~=1.4.1",
+            "dash-bootstrap-components~=1.0.0",
+        ],
+        "pre-commit": [
+            "pre-commit",
+        ],
+        "test": [
+            "pytest",
+            "tox",
+        ],
+    },
+    cmdclass={"develop": PostDevelopCommand},
+)
